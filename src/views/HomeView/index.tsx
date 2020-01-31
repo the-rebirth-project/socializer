feaef660266f1d9015484a7f51c1411123885980
SKP <skp.frl@gmail.com>
2020-01-31 21:25:54 +0530

    :sparkles: Add HomeView

--- src/views/HomeView/index.tsx
+++ src/views/HomeView/index.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { RouteComponentProps } from '@reach/router';
 
 export const HomeView: React.FC<RouteComponentProps> = () => {
-  return <div></div>;
+  return <div>Home</div>;
 };
