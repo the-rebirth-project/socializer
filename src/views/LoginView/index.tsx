c1bb178c9c972aef78ce8b09d45e355bf578540a
SKP <skp.frl@gmail.com>
2020-01-31 21:26:08 +0530

    :sparkles: Add LoginView

--- src/views/LoginView/index.tsx
+++ src/views/LoginView/index.tsx
@@ -1,6 +1,24 @@
 import React from 'react';
 import { RouteComponentProps } from '@reach/router';
+import { LoginForm } from '../../components/specific/LoginForm';
+import { SubHeading } from '../../components/shared/SubHeading';
+import { FancyButton } from '../../components/shared/FancyButton';
+import { SmallText } from '../../components/shared/SmallText';
+import { LinkText } from '../../components/shared/LinkText';
+import { Wrapper, ButtonContainer } from '../RegisterView/styles';
 
 export const LoginView: React.FC<RouteComponentProps> = () => {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <SubHeading>Enter Credentials</SubHeading>
+      <LoginForm />
+      <ButtonContainer>
+        <FancyButton>Sign In</FancyButton>
+        <SmallText>
+          Don't have an account? Click <LinkText to='/register'>here</LinkText>{' '}
+          to sign up
+        </SmallText>
+      </ButtonContainer>
+    </Wrapper>
+  );
 };
