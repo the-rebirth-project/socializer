3a2d7fca02f8ac5879c1fd434b3d2e8854a21e63
SKP <skp.frl@gmail.com>
2020-01-31 21:22:21 +0530

    :sparkles: Add SmallText component

---
+++ src/components/shared/SmallText/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Wrapper } from './styles';
+
+export const SmallText: React.FC = ({ children }) => {
+  return <Wrapper>{children}</Wrapper>;
+};
---
+++ src/components/shared/SmallText/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.span`
+  color: ${props => props.theme.colors.textColor};
+  font-size: 1.2rem;
+  opacity: 0.8;
+`;
