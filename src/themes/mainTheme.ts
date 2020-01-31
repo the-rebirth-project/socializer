845793e340e407b7d1781e6f4f77f7fc9fb5e722
SKP <skp.frl@gmail.com>
2020-01-31 21:20:57 +0530

    :lipstick: Add main theme

---
+++ src/themes/mainTheme.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from 'styled-components';
+
+export const mainTheme: DefaultTheme = {
+  colors: {
+    primary: '#8000FF',
+    secondary: '#56AAFF',
+    tertiary: '#FF006F',
+    background: '#202020',
+    cardBackground: '#333333',
+    textColor: '#fff'
+  }
+};
