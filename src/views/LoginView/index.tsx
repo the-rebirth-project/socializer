34fddc7291eb4d43a8cc470fb784b8b36b35bd25
SKP <skp.frl@gmail.com>
2020-02-01 23:09:23 +0530

    :lipstick: Refactor layout

--- src/views/LoginView/index.tsx
+++ src/views/LoginView/index.tsx
@@ -2,14 +2,7 @@ import React from 'react';
 import { RouteComponentProps } from '@reach/router';
 import { LoginForm } from '../../components/specific/LoginForm';
 import { SubHeading } from '../../components/shared/SubHeading';
-import { FancyButton } from '../../components/shared/FancyButton';
-import { SmallText } from '../../components/shared/SmallText';
-import { LinkText } from '../../components/shared/LinkText';
-import {
-  Wrapper,
-  ButtonContainer,
-  GlobalBackground
-} from '../RegisterView/styles';
+import { Wrapper, GlobalBackground } from '../RegisterView/styles';
 
 export const LoginView: React.FC<RouteComponentProps> = () => {
   return (
@@ -18,13 +11,6 @@ export const LoginView: React.FC<RouteComponentProps> = () => {
       <Wrapper>
         <SubHeading>Enter Credentials</SubHeading>
         <LoginForm />
-        <ButtonContainer>
-          <FancyButton>Sign In</FancyButton>
-          <SmallText>
-            Don't have an account? Click{' '}
-            <LinkText to='/register'>here</LinkText> to sign up
-          </SmallText>
-        </ButtonContainer>
       </Wrapper>
     </>
   );
