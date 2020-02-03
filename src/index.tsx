1c67fb1c9d0072f0cc8e0b3827ecb500d9a3ca74
SKP <skp.frl@gmail.com>
2020-02-03 22:31:39 +0530

    :sparkles: Implement firebase

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import * as serviceWorker from './serviceWorker';
 import { ThemeProvider } from 'styled-components';
 import { mainTheme } from './themes/mainTheme';
+
+const FBConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+firebase.initializeApp(FBConfig);
 
 ReactDOM.render(
   <ThemeProvider theme={mainTheme}>
