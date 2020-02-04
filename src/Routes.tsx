91410788488236eab4eef88e6883a9ef5336246f
SKP <skp.frl@gmail.com>
2020-02-04 22:14:52 +0530

    :sparkles: Implement a redirect route

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Router } from '@reach/router';
+import { RedirectView } from './views/RedirectView';
 import { LoginView } from './views/LoginView';
 import { RegisterView } from './views/RegisterView';
 import { HomeView } from './views/HomeView';
@@ -7,7 +8,8 @@ import { HomeView } from './views/HomeView';
 export const Routes: React.FC = () => {
   return (
     <Router>
-      <HomeView path='/' />
+      <RedirectView path='/' />
+      <HomeView path='/home' />
       <LoginView path='/login' />
       <RegisterView path='/register' />
     </Router>
