016e74710c82bec132b47d45516ce4be5ea3e9d1
SKP <skp.frl@gmail.com>
2020-02-04 22:17:08 +0530

    :sparkles: Add PostItem component

---
+++ src/components/specific/PostItem/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Wrapper } from './styles';
+
+export const PostItem: React.FC = () => {
+  return <Wrapper>{/* PostItem components here */}</Wrapper>;
+};
---
+++ src/components/specific/PostItem/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.section``;
+export const Comment = styled.div`
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  row-gap: 1rem;
+`;
