0988ed4e48129b67ae7094f5ef81afafeaaf9a58
SKP <skp.frl@gmail.com>
2020-02-08 21:24:23 +0530

    :label: Add properties for comments and likes

--- src/types/Post.ts
+++ src/types/Post.ts
@@ -1,7 +1,14 @@
+import { Like } from './Like';
+import { Comment } from './Comment';
+
 export type Post = {
   postId: string; // a post id is attached every time we get a post. required for making requests
   body: string;
   userHandle: string;
   createdAt: string;
   userProfile: string;
+  comments: Comment[];
+  likes: Like[];
+  // used for disabling inputs and notifying the user when the comment gets added to the db
+  postingComment?: boolean;
 };
