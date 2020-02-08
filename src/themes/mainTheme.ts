b90c4575dc291d81bca3a7810b3b83a7b75eb4ae
SKP <skp.frl@gmail.com>
2020-02-08 21:25:09 +0530

    :lipstick: Add secondary colors

--- src/themes/mainTheme.ts
+++ src/themes/mainTheme.ts
@@ -5,6 +5,8 @@ export const mainTheme: DefaultTheme = {
     primary: '#8000FF',
     secondary: '#56AAFF',
     tertiary: '#FF006F',
+    primary2: '#0DFFD6',
+    secondary2: '#DBFB00',
     background: '#202020',
     cardBackground: '#333333',
     textColor: '#fff'
