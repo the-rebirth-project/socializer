366f5380e50bfe87cb64178f4a5fe38958f8989b
SKP <skp.frl@gmail.com>
2020-02-08 21:19:38 +0530

    :sparkles: Add Navbar

---
+++ src/components/shared/Navbar/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text } from '../../shared/Text';
+import { Wrapper, FlexContainer, TitleContainer } from './styles';
+
+export const Navbar: React.FC = () => {
+  return (
+    <Wrapper>
+      <FlexContainer>
+        <TitleContainer>
+          <Text size={1.8} weight={700}>
+            <h1>socializer</h1>
+          </Text>
+        </TitleContainer>
+      </FlexContainer>
+    </Wrapper>
+  );
+};
+
+export const NavbarBottomMargin = 7; // in rem
---
+++ src/components/shared/Navbar/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.nav`
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  width: 100vw;
+  height: 4rem;
+  background-image: linear-gradient(
+    to right,
+    ${props => props.theme.colors.primary},
+    ${props => props.theme.colors.secondary}
+  );
+
+  box-shadow: 0px 3px 6px ${props => props.theme.colors.primary}40;
+`;
+
+export const FlexContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+`;
+
+export const TitleContainer = styled.div`
+  margin: 0 auto;
+`;
