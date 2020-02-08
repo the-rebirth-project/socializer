881e87ebc32f20cedea1bb6ab7bd682762a44730
SKP <skp.frl@gmail.com>
2020-02-08 21:21:24 +0530

    :sparkles: Add Comments component

---
+++ src/components/specific/Comments/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.section`
+  display: grid;
+  grid-template-rows: repeat(2, min-content);
+  row-gap: 0.7rem;
+  width: 100%;
+`;
+
+export const CommentContainer = styled.p`
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  word-break: break-word;
+  width: 100%;
+  line-height: 2rem;
+`;
