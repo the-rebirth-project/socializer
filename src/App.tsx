9ce03fbec23cd80fc860219b15c4fc87706799df
SKP <skp.frl@gmail.com>
2020-02-08 21:23:30 +0530

    :sparkles: Set user state on component mount

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import React, { useEffect } from 'react';
+import axios from 'axios';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 import { createGlobalStyle } from 'styled-components';
+import { useUserDispatch } from './contexts/UserContext';
+import { API_URL } from './constants/apiUrl';
 import { Routes } from './Routes';
 
 const GlobalStyle = createGlobalStyle`
@@ -17,6 +22,29 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 const App: React.FC = () => {
+  const dispatch = useUserDispatch();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(async user => {
+      if (user) {
+        const idToken = await user.getIdToken();
+        const res = await axios.get(`${API_URL}/user`, {
+          headers: {
+            Authorization: `Bearer ${idToken}`
+          }
+        });
+        console.log(res.data);
+        dispatch({
+          type: 'SET_USER',
+          payload: {
+            userHandle: res.data.userHandle,
+            posts: res.data.posts
+          }
+        });
+      }
+    });
+  }, [dispatch]);
+
   return (
     <div>
       <GlobalStyle />
