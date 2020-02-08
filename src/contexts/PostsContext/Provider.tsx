8bb82c76276c8b9d0e5d43c43765d22e258867ab
SKP <skp.frl@gmail.com>
2020-02-08 21:27:27 +0530

    :truck: Move imports

--- src/contexts/PostsContext/Provider.tsx
+++ src/contexts/PostsContext/Provider.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import { postsReducer } from './reducers';
 import {
   PostsStateContext,
   PostsDispatchContext,
-  postsReducer,
   initialState
 } from './context';
 
