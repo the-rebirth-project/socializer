8a91b3edb3f9f939e21b181f97dff7640a8d81f7
SKP <skp.frl@gmail.com>
2020-02-08 21:24:44 +0530

    :label: Add more color definitions

--- src/types/styled.d.ts
+++ src/types/styled.d.ts
@@ -4,7 +4,9 @@ declare module 'styled-components' {
   export interface DefaultTheme {
     colors: {
       primary: string;
+      primary2: string;
       secondary: string;
+      secondary2: string;
       tertiary: string;
       background: string;
       cardBackground: string;
