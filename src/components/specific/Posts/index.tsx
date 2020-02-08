34d2e032f666e847934245f08f0513ffdc7616e8
SKP <skp.frl@gmail.com>
2020-02-08 21:29:04 +0530

    :sparkles: Implement functionality to get posts from db and display them

--- src/components/specific/Posts/index.tsx
+++ src/components/specific/Posts/index.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
-import { Wrapper } from './styles';
+import React, { useEffect, useCallback } from 'react';
+import axios from 'axios';
+import { API_URL } from '../../../constants/apiUrl';
+import {
+  usePostsState,
+  usePostsDispatch
+} from '../../../contexts/PostsContext';
+import { PostItem } from '../PostItem';
+import { Post } from '../../../types/Post';
+import { Wrapper, GridContainer } from './styles';
 
 // A posts component which will render an individual PostItem comprising of the post body, comments, replies, etc
 // We'll use react contexts here instead of just prop drilling as we'll have multiple nested components requiring the same post data
 
 export const Posts: React.FC = () => {
-  return <Wrapper></Wrapper>;
+  const state = usePostsState();
+  const dispatch = usePostsDispatch();
+
+  const getPosts = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/posts`);
+      const postsData: Post[] = response.data;
+      console.log(postsData);
+      dispatch({ type: 'SET_POSTS', payload: postsData });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  const memoizedGetPosts = useCallback(getPosts, []);
+
+  useEffect(() => {
+    memoizedGetPosts();
+  }, [memoizedGetPosts]);
+
+  return (
+    <Wrapper>
+      <GridContainer>
+        {state.posts.map(p => (
+          <PostItem post={p} key={p.postId} />
+        ))}
+      </GridContainer>
+    </Wrapper>
+  );
 };
