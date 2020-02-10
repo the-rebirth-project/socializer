11bd2b51e4cb62a24fd14b2dd4c333e39f940fc3
SKP <skp.frl@gmail.com>
2020-02-10 21:01:46 +0530

    :recycle: Refactor OpacityLoader component

--- src/components/shared/OpacityLoader/index.tsx
+++ src/components/shared/OpacityLoader/index.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Wrapper } from './styles';
 
 type OpacityLoaderProps = {
-  loading?: boolean;
+  loading: boolean | undefined;
   defaultOpacity: number; // the opacity to use once loading is done i.e element's default state
+  loadingOpacity?: number; // opacity to show once loading is done
 };
 
 // This component makes elements less opaque if loading is true. Used as a loader when making api requests
@@ -11,10 +12,15 @@ type OpacityLoaderProps = {
 export const OpacityLoader: React.FC<OpacityLoaderProps> = ({
   children,
   loading,
-  defaultOpacity
+  defaultOpacity,
+  loadingOpacity
 }) => {
   return (
-    <Wrapper loading={loading} defaultOpacity={defaultOpacity}>
+    <Wrapper
+      loading={loading}
+      defaultOpacity={defaultOpacity}
+      loadingOpacity={loadingOpacity}
+    >
       {children}
     </Wrapper>
   );
--- src/components/shared/OpacityLoader/styles.ts
+++ src/components/shared/OpacityLoader/styles.ts
@@ -1,10 +1,23 @@
 import styled from 'styled-components';
 
 type WrapperProps = {
-  loading?: boolean;
+  loading: boolean | undefined;
   defaultOpacity: number;
+  loadingOpacity?: number;
 };
+
+// returns the prop value if it is defined
+const propIfDefined = (
+  prop: number | undefined,
+  defaultVal: number | string
+): number | string => {
+  return prop ? prop : defaultVal;
+};
+
 export const Wrapper = styled.div<WrapperProps>`
-  opacity: ${props => (props.loading ? 0.7 : props.defaultOpacity)} !important;
+  opacity: ${props =>
+    props.loading
+      ? propIfDefined(props.loadingOpacity, 0.7)
+      : props.defaultOpacity} !important;
   transition: all 0.2s ease-in-out;
 `;
