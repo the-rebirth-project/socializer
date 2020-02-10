5c78703894b374ee1b514ca52da90fbae2132ac2
SKP <skp.frl@gmail.com>
2020-02-10 21:02:46 +0530

    :lipstick: Refactor styles

--- src/views/HomeView/styles.ts
+++ src/views/HomeView/styles.ts
@@ -1,9 +1,28 @@
 import styled, { createGlobalStyle } from 'styled-components';
 
-export const Wrapper = styled.div``;
 export const GlobalStyles = createGlobalStyle`
   body {
     overflow-x: hidden;
     background-color: ${props => props.theme.colors.background};
+    line-height: 1.35;
+
+    p {
+      line-height: 1.45;
+    }
+    
   }
+`;
+
+export const ContentWrapper = styled.main`
+  width: 100vw;
+  height: fit-content;
+  display: flex;
+  justify-content: center;
+`;
+
+export const GridContainer = styled.div`
+  display: grid;
+  grid-auto-rows: min-content;
+  row-gap: 4rem;
+  grid-template-columns: 32rem;
 `;
