55978d3d22618aaa47daa38172dd733acca73449
SKP <skp.frl@gmail.com>
2020-02-10 21:01:01 +0530

    :sparkles: Add PostAdd component

---
+++ src/components/specific/PostAdd/index.tsx
@@ -0,0 +1,139 @@
+import React, { useState } from 'react';
+import { navigate } from '@reach/router';
+import axios from 'axios';
+import firebase from 'firebase/app';
+import uuid from 'uuid/v4';
+import 'firebase/auth';
+import { useUserState } from '../../../contexts/UserContext';
+import {
+  usePostsDispatch,
+  usePostsState
+} from '../../../contexts/PostsContext';
+import { GradientBox } from '../../shared/GradientBox';
+import { CircleAvatar } from '../../shared/CircleAvatar';
+import { FancyButton } from '../../shared/FancyButton';
+import { OpacityLoader } from '../../shared/OpacityLoader';
+import { Text } from '../../shared/Text';
+import { Post } from '../../../types';
+import {
+  Wrapper,
+  UserHeader,
+  UsernameContainer,
+  Body,
+  StyledTextArea,
+  PostButtonContainer
+} from './styles';
+import { API_URL } from '../../../constants/apiUrl';
+
+export const PostAdd: React.FC = () => {
+  const userState = useUserState();
+  const postsDispatch = usePostsDispatch();
+  const postsState = usePostsState();
+  const [postBody, setPostBody] = useState('');
+  /* 
+    checks to see if any posts are being added
+    keep in mind that all newly added posts are added to the start of the posts arr
+  */
+  const isPostBeingAdded =
+    postsState.posts[0] && postsState.posts[0].addingPost;
+
+  const onTextAreaChanged = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setPostBody(e.target.value);
+  };
+
+  const onPostSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!isPostBeingAdded) {
+      /* 
+        Fields like createdAt and postId are "temporary" and are only added to obey the type definitions of Post
+        Once we add the new post to the firebase db, we'll be fetching that post with the firebase provided postId and createdAt
+      */
+      // pass value of postBody to another variable as we'll reset the input state on submit
+      const postInput = postBody;
+      setPostBody('');
+
+      const postId = uuid();
+      const newPost: Post = {
+        body: postInput,
+        comments: [],
+        likes: [],
+        createdAt: new Date().toISOString(),
+        postId,
+        userHandle: userState.userHandle,
+        userProfile: userState.userProfile
+      };
+      postsDispatch({ type: 'ADD_POST', payload: newPost });
+
+      try {
+        firebase.auth().onAuthStateChanged(async user => {
+          if (user) {
+            postsDispatch({
+              type: 'SET_ADDING_POST',
+              payload: {
+                postId,
+                value: true
+              }
+            });
+            const idToken = await user.getIdToken();
+            await axios.post(
+              `${API_URL}/posts`,
+              {
+                body: postInput
+              },
+              {
+                headers: {
+                  Authorization: `Bearer ${idToken}`
+                }
+              }
+            );
+            postsDispatch({
+              type: 'SET_ADDING_POST',
+              payload: {
+                postId,
+                value: false
+              }
+            });
+          } else {
+            navigate('/login');
+          }
+        });
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  return (
+    <Wrapper>
+      <GradientBox>
+        <UserHeader>
+          <CircleAvatar imgUrl={userState.userProfile} />
+          <UsernameContainer>
+            <Text size={1.6} weight={700}>
+              {userState.userHandle}
+            </Text>
+          </UsernameContainer>
+        </UserHeader>
+      </GradientBox>
+
+      <Body>
+        <form onSubmit={onPostSubmit}>
+          <StyledTextArea
+            id='addpost'
+            placeholder='Add a post'
+            maxRows={10}
+            maxLength={1000}
+            value={postBody}
+            onChange={onTextAreaChanged}
+            required
+          />
+          <OpacityLoader loading={isPostBeingAdded} defaultOpacity={1}>
+            <PostButtonContainer>
+              <FancyButton type='submit'>Post</FancyButton>
+            </PostButtonContainer>
+          </OpacityLoader>
+        </form>
+      </Body>
+    </Wrapper>
+  );
+};
---
+++ src/components/specific/PostAdd/styles.ts
@@ -0,0 +1,73 @@
+import styled from 'styled-components';
+import TextareaAutosize from 'react-textarea-autosize';
+
+export const Wrapper = styled.section`
+  width: 32rem;
+  margin: 0 auto;
+  display: grid;
+  grid-template-rows: min-content 1fr;
+`;
+
+export const UserHeader = styled.main`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 7fr;
+  column-gap: 1rem;
+  padding: 0rem 1rem;
+`;
+
+export const UsernameContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Body = styled.main`
+  width: 100%;
+  height: auto;
+  background-color: ${props => props.theme.colors.cardBackground};
+  border-bottom-left-radius: 1rem;
+  border-bottom-right-radius: 1rem;
+  padding: 1.5rem 1rem;
+  padding-bottom: 4rem;
+  position: relative;
+`;
+
+export const StyledTextArea = styled(TextareaAutosize)`
+  width: 100%;
+  border: none;
+  background-color: transparent;
+  padding: 0rem;
+  font-family: inherit;
+  font-size: 1.6rem;
+  color: ${props => props.theme.colors.textColor};
+  resize: none;
+  padding-right: 0.5rem;
+  line-height: 1.45;
+  margin-bottom: 2rem;
+
+  :focus {
+    outline: rgba(255, 255, 255, 0);
+  }
+
+  ::-webkit-scrollbar {
+    width: 0.5rem;
+  }
+
+  ::-webkit-scrollbar-track {
+    box-shadow: inset 0 0 6px rgba(255, 255, 255, 0.3);
+    border-radius: 10px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border-radius: 2rem;
+    background-color: ${props => props.theme.colors.textColor};
+  }
+`;
+
+export const PostButtonContainer = styled.div`
+  position: absolute;
+  bottom: -2rem;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 90%;
+`;
