44de0f34400d639339427343130fd429489b4d99
SKP <skp.frl@gmail.com>
2020-02-13 20:06:44 +0530

    :recycle: Refactor LoadingSpinner component

--- src/components/shared/LoadingSpinner/index.tsx
+++ src/components/shared/LoadingSpinner/index.tsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import { Wrapper, Spinner } from './styles';
+import { Spinner, Wrapper } from './styles';
 
 type LoadingSpinnerProps = {
-  loading: boolean;
-  alignCenter?: boolean;
+  loading: number; // 1 for true and 0 for false
+  centerSpinner?: boolean; // centers spinner as a fixed element
+  small?: boolean;
 };
 
 export const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({
   loading,
-  alignCenter,
+  centerSpinner,
+  small,
   children
 }) => {
+  const SpinnerElement = <Spinner small={small}></Spinner>;
+
   return (
     <>
-      {loading && (
-        <Wrapper alignCenter={alignCenter}>
-          <Spinner></Spinner>
-        </Wrapper>
-      )}
+      {loading === 1 &&
+        (centerSpinner ? <Wrapper>{SpinnerElement}</Wrapper> : SpinnerElement)}
 
       {!loading && <>{children}</>}
     </>
