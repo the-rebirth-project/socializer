127e7a38fd98ec80515da26ad0f8f50390347d34
SKP <skp.frl@gmail.com>
2020-02-13 20:15:46 +0530

    :art: Add state to keep track of posts being fetched

--- src/contexts/PostsContext/context.ts
+++ src/contexts/PostsContext/context.ts
@@ -5,10 +5,12 @@ import { Post } from '../../types';
 type Dispatch = (action: Action) => void;
 export type State = {
   posts: Post[];
+  fetchingPosts: boolean;
 };
 
 export const initialState = {
-  posts: []
+  posts: [],
+  fetchingPosts: false
 };
 
 export const PostsStateContext = React.createContext<State | undefined>(
@@ -18,14 +20,14 @@ export const PostsDispatchContext = React.createContext<Dispatch | undefined>(
   undefined
 );
 
-export const usePostsState: () => State = () => {
+export const usePostsState = (): State => {
   const context = React.useContext(PostsStateContext);
   if (context === undefined)
     throw new Error('Context must be used within a PostsProvider');
   return context;
 };
 
-export const usePostsDispatch = () => {
+export const usePostsDispatch = (): Dispatch => {
   const context = React.useContext(PostsDispatchContext);
   if (context === undefined)
     throw new Error('Context must be used within a PostsProvider');
