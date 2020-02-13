45ddeb8c555c1161ca00808871b42df1feeb74e5
SKP <skp.frl@gmail.com>
2020-02-13 20:20:22 +0530

    :label: Update type definitions for Comment

--- src/types/Comment.ts
+++ src/types/Comment.ts
@@ -1,5 +1,14 @@
+import { Reply } from './Reply';
+
 export type Comment = {
+  id: string;
   userHandle: string;
   body: string;
   createdAt: string;
+  numReplies: number;
+  replies: Reply[];
+  showReplies: boolean;
+  localReplies: Reply[]; // these are replies that are added while fetchedReplies is false
+  postingReply: boolean;
+  fetchedReplies: boolean;
 };
