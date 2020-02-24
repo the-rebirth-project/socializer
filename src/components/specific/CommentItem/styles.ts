fcd714cbe9e642664c8ec4ec271ff41ba0ac351c
SKP <skp.frl@gmail.com>
2020-02-24 22:17:14 +0530

    :lipstick: Style CommentItem component

--- src/components/specific/CommentItem/styles.ts
+++ src/components/specific/CommentItem/styles.ts
@@ -2,21 +2,29 @@ import styled from 'styled-components';
 import { LoadingSpinner } from '../../shared/LoadingSpinner';
 
 export const Wrapper = styled.div`
-  display: flex;
+  display: grid;
+  row-gap: 0.1rem;
   flex-direction: column;
   flex-wrap: wrap;
   word-break: break-word;
   width: 100%;
-  line-height: 2rem;
+  line-height: 1.4;
 `;
 
+// wraps around the loading spinner too
 export const RepliesContainer = styled.div`
   border-left: 1px solid ${props => props.theme.colors.textColor};
-  display: block;
   margin-left: 0.5rem;
   padding-left: 1rem;
   margin-top: 0.5rem;
   position: relative;
+`;
+
+// wraps around the list of replies
+export const RepliesWrapper = styled.section`
+  display: grid;
+  grid-auto-rows: min-content;
+  row-gap: 0.5rem;
 `;
 
 export const ClickableSpan = styled.span`
