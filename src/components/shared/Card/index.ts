3d5b117e76a523f241db2b011f3d7d7c1fb7edc2
SKP <skp.frl@gmail.com>
2020-02-24 22:08:06 +0530

    :sparkles: Add Card shared component

---
+++ src/components/shared/Card/index.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Card = styled.div`
+  background-color: rgba(51, 51, 51, 0.6);
+  color: ${props => props.theme.colors.textColor};
+  border-radius: 1rem;
+  padding: 1rem;
+`;
