fc4fa796a5cd76e21406769d91a0cd9738c4fa88
SKP <skp.frl@gmail.com>
2020-02-24 22:03:25 +0530

    :label: Add UserData type

---
+++ src/types/UserData.ts
@@ -0,0 +1,9 @@
+export interface UserData {
+  userHandle: string;
+  userProfileImg: string;
+  bio: string;
+  location: string;
+  numConnects: number; // number of followers essentially
+  numSeeds: number;
+  numPosts: number;
+}
