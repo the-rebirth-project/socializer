2ff43c6f308a09955da9d17c15452d5a671f13ae
SKP <skp.frl@gmail.com>
2020-02-24 22:18:29 +0530

    :recycle: Refactor Comments component to use CommentsContext

--- src/components/specific/Comments/index.tsx
+++ src/components/specific/Comments/index.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { CommentItem } from '../CommentItem';
 import { Text } from '../../shared/Text';
+import {
+  useCommentsState,
+  useCommentsDispatch
+} from '../../../contexts/CommentsContext';
 import { Post } from '../../../types/Post';
 import { Wrapper, CommentsContainer } from './styles';
 
@@ -9,18 +13,26 @@ type CommentsProps = {
 };
 
 export const Comments: React.FC<CommentsProps> = ({ post }) => {
+  const commentsState = useCommentsState();
+  const commentsDispatch = useCommentsDispatch();
+
+  useEffect(() => {
+    commentsDispatch({ type: 'SET_COMMENTS', payload: post.comments });
+  }, [commentsDispatch, post.comments]);
+
   return (
     <Wrapper>
       <Text size={1.3} opacity={0.7}>
         Comments ({post.comments.length})
       </Text>
       <CommentsContainer>
-        {post.comments.map((comment, idx) => (
+        {commentsState.comments.map((comment, idx) => (
           <CommentItem
-            post={post}
             comment={comment}
             index={idx}
             key={comment.id}
+            numComments={commentsState.comments.length}
+            postId={post.postId}
           />
         ))}
       </CommentsContainer>
