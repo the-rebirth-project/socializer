2e5e51631bde4c9db9d63a963ae17b693c914503
SKP <skp.frl@gmail.com>
2020-03-14 16:00:39 +0530

    :label: Add type definitions for SetFetchedReplies, SetFetchingReplies and ShowReplies actions

--- src/contexts/RepliesContext/actionTypes.ts
+++ src/contexts/RepliesContext/actionTypes.ts
@@ -5,11 +5,29 @@ export type Action =
   | AddReply
   | AddLocalReply
   | SetPostingReply
-  | SetShowReplyForm;
+  | SetFetchingReplies
+  | SetFetchedReplies
+  | ResetLocalReplies
+  | ShowReplies;
 
 type SetReplies = {
   type: 'SET_REPLIES';
   payload: Reply[];
+};
+
+type SetFetchedReplies = {
+  type: 'SET_FETCHED_REPLIES';
+  payload: boolean;
+};
+
+type SetFetchingReplies = {
+  type: 'SET_FETCHING_REPLIES';
+  payload: boolean;
+};
+
+type ShowReplies = {
+  type: 'SHOW_REPLIES';
+  payload: boolean;
 };
 
 type AddReply = {
