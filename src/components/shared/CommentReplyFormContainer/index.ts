2ee2acb93b5f60670b1ce113ffe209a3115903db
SKP <skp.frl@gmail.com>
2020-03-14 15:17:26 +0530

    :sparkles: Add shared components used in both comments and replies component trees

---
+++ src/components/shared/CommentReplyFormContainer/index.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const CommentReplyFormContainer = styled.div`
+  display: grid;
+  grid-template-columns: 7fr min-content;
+  column-gap: 1rem;
+  align-items: flex-end;
+  margin-bottom: 1rem;
+  margin-right: 0.5rem;
+  border-radius: 1rem;
+  margin-top: 0.5rem;
+`;
---
+++ src/components/shared/CommentReplyWrapper/index.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const CommentReplyWrapper = styled.main`
+  display: grid;
+  grid-auto-rows: min-content;
+  row-gap: 0.5rem;
+  flex-direction: column;
+  flex-wrap: wrap;
+  word-break: break-word;
+  width: 100%;
+  line-height: 1.35;
+`;
