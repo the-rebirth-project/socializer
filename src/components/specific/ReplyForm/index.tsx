26c9c89301b0b9f6de39fe130f4ceca729ff7ee2
SKP <skp.frl@gmail.com>
2020-03-14 15:55:20 +0530

    :sparkles: Finish ReplyForm specific component

--- src/components/specific/ReplyForm/index.tsx
+++ src/components/specific/ReplyForm/index.tsx
@@ -1,24 +1,103 @@
 import React, { useState } from 'react';
-import { Wrapper, TextArea } from './styles';
+import uuid from 'uuid/v4';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import {
+  useRepliesState,
+  useRepliesDispatch
+} from '../../../contexts/RepliesContext';
+import { useUserState } from '../../../contexts/UserContext';
+import { Text } from '../../shared/Text';
+import { CommentReplyTextArea } from '../../shared/CommentReplyTextArea';
+import { CommentReplyFormContainer } from '../../shared/CommentReplyFormContainer';
+import { OpacityLoader } from '../../shared/OpacityLoader';
+import { SendButton } from '../../shared/SendButton';
+import { regularTextSize } from '../../../constants';
+import { Reply } from '../../../types';
+import { Wrapper } from './styles';
 
-export const ReplyForm: React.FC = () => {
+type ReplyFormProps = {
+  commentUserHandle: string;
+  postId: string;
+  commentId: string;
+  postUserHandle: string;
+};
+
+export const ReplyForm: React.FC<ReplyFormProps> = ({
+  commentUserHandle,
+  postId,
+  commentId,
+  postUserHandle
+}) => {
+  const db = firebase.firestore();
+  const repliesState = useRepliesState();
+  const repliesDispatch = useRepliesDispatch();
+  const { userHandle } = useUserState();
   const [replyBody, setReplyBody] = useState('');
 
   const onTextAreaChanged = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setReplyBody(e.target.value);
   };
 
+  const onReplySubmit = async () => {
+    if (replyBody) {
+      const replyInput = replyBody;
+      setReplyBody('');
+
+      const newReplyId = uuid();
+      const newReply: Reply = {
+        body: replyInput,
+        createdAt: new Date().toISOString(),
+        id: newReplyId,
+        userHandle
+      };
+
+      repliesDispatch({ type: 'ADD_REPLY', payload: newReply });
+
+      if (!repliesState.showReplies)
+        repliesDispatch({ type: 'ADD_LOCAL_REPLY', payload: newReply });
+
+      repliesDispatch({ type: 'SET_POSTING_REPLY', payload: true });
+
+      try {
+        await db
+          .doc(`users/${postUserHandle}/posts/${postId}/comments/${commentId}`)
+          .collection('replies')
+          .doc(newReplyId)
+          .set(newReply);
+      } catch (err) {
+        // TODO: Handle error
+        console.log(err);
+      }
+
+      repliesDispatch({ type: 'SET_POSTING_REPLY', payload: false });
+    }
+  };
+
   return (
     <Wrapper>
-      <TextArea
-        maxLength={1500}
-        maxRows={5}
-        value={replyBody}
-        onChange={onTextAreaChanged}
-        // TODO: Substitute 'username here' for the userHandle you're replying to
-        placeholder={`Replying to username here`}
-        required
-      />
+      <OpacityLoader
+        loading={repliesState.postingReply ? 1 : 0}
+        defaultOpacity={1}
+      >
+        <Text size={regularTextSize} weight={700}>
+          {userHandle}
+        </Text>
+        <CommentReplyFormContainer>
+          <CommentReplyTextArea
+            maxLength={1500}
+            maxRows={5}
+            value={replyBody}
+            onChange={onTextAreaChanged}
+            placeholder={`Replying to ${commentUserHandle}`}
+            required
+          />
+          <SendButton
+            onClick={onReplySubmit}
+            disabled={repliesState.postingReply}
+          />
+        </CommentReplyFormContainer>
+      </OpacityLoader>
     </Wrapper>
   );
 };
