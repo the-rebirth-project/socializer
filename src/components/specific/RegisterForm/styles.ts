b8511c1a5bfb9ec2e97692aaa3e507273943cecf
SKP <skp.frl@gmail.com>
2020-03-14 15:54:28 +0530

    :recycle: Refactor RegisterForm styles

--- src/components/specific/RegisterForm/styles.ts
+++ src/components/specific/RegisterForm/styles.ts
@@ -8,23 +8,6 @@ export const FormWrapper = styled.form`
   justify-content: center;
 `;
 
-export const FormFieldGrid = styled.div`
-  display: grid;
-  grid-template-rows: 1fr 3fr;
-  row-gap: 0.7rem;
-`;
-
-export const ErrorMessage = styled.span`
-  color: ${props => props.theme.colors.tertiary};
-  font-size: 1.2rem;
-`;
-
-export const SubmitErrorContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  min-height: 1.3rem;
-`;
-
 export const ButtonContainer = styled.div`
   display: grid;
   height: 100%;
@@ -32,10 +15,4 @@ export const ButtonContainer = styled.div`
   grid-template-rows: 1fr 1fr;
   row-gap: 1rem;
   grid-area: 6 / 1 / 7 / 2;
-`;
-
-export const CheckboxFieldGrid = styled.div`
-  margin-left: 1rem;
-  display: grid;
-  grid-template-columns: 0.09fr 3fr;
 `;
