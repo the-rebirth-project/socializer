d3367b9bc89a5a6edecb1506d1797ff503ef31a6
SKP <skp.frl@gmail.com>
2020-03-14 15:16:36 +0530

    :building_construction: Move styled components frequently used in forms to FormStyles

---
+++ src/components/shared/FormStyles/index.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const FormFieldGrid = styled.div`
+  display: grid;
+  grid-template-rows: 1fr 3fr;
+  row-gap: 0.7rem;
+`;
+
+export const ErrorMessage = styled.span`
+  color: ${props => props.theme.colors.tertiary};
+  font-size: 1.2rem;
+`;
+
+export const MetaTextContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  min-height: 1.3rem;
+`;
+
+export const CheckboxFieldGrid = styled.div`
+  margin-left: 1rem;
+  display: grid;
+  grid-template-columns: 0.09fr 3fr;
+`;
