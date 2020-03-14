bf91e8ea9565dc2a69efc6c87248b05d10ce501c
SKP <skp.frl@gmail.com>
2020-03-14 16:33:30 +0530

    :label: Add type definitions for SetFetchingData and SetIsSubscribed actions in UserProfileContext

--- src/contexts/UserProfileContext/actionTypes.ts
+++ src/contexts/UserProfileContext/actionTypes.ts
@@ -1,8 +1,18 @@
 import { UserData } from '../../types/UserData';
 
-export type Action = SetUserProfile;
+export type Action = SetUserProfile | SetFetchingData | SetIsSubscribed;
 
 type SetUserProfile = {
   type: 'SET_USER_PROFILE';
   payload: UserData;
+};
+
+type SetFetchingData = {
+  type: 'SET_FETCHING_DATA';
+  payload: boolean;
+};
+
+type SetIsSubscribed = {
+  type: 'SET_IS_SUBSCRIBED';
+  payload: boolean;
 };
