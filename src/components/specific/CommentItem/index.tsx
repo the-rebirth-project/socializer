d92df71b9c7f26900b0d33a8c7212f5ee2f56f9c
SKP <skp.frl@gmail.com>
2020-03-14 15:48:09 +0530

    :recycle: Refactor CommentItem component

--- src/components/specific/CommentItem/index.tsx
+++ src/components/specific/CommentItem/index.tsx
@@ -1,100 +1,81 @@
-import React, { useState } from 'react';
+import React from 'react';
 import moment from 'moment';
-import uuid from 'uuid/v4';
-import { usePostsDispatch, getReplies } from '../../../contexts/PostsContext';
-import { useTextInputDispatch } from '../../../contexts/TextInputContext';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 import { useCommentsState } from '../../../contexts/CommentsContext';
-import { ReplyForm } from '../ReplyForm';
-import { Text } from '../../shared/Text';
-import { OpacityLoader } from '../../shared/OpacityLoader';
-import { Comment, CommentMode, Reply } from '../../../types';
 import {
-  Wrapper,
-  RepliesContainer,
-  RepliesWrapper,
-  ClickableSpan,
-  StyledLoadingSpinner
-} from './styles';
+  useRepliesState,
+  useRepliesDispatch
+} from '../../../contexts/RepliesContext';
+import { Replies } from '../Replies';
+import { Text } from '../../shared/Text';
+import { CommentReplyWrapper } from '../../shared/CommentReplyWrapper';
+import { OpacityLoader } from '../../shared/OpacityLoader';
+import { Comment, Reply } from '../../../types';
+import { ClickableSpan } from './styles';
 
 type CommentItemProps = {
-  postId: string;
-  numComments: number;
+  isLastComment: boolean;
   comment: Comment;
-  index: number;
+  postId: string;
+  postUserHandle: string;
 };
 
 export const CommentItem: React.FC<CommentItemProps> = ({
-  postId,
   comment,
-  numComments,
-  index
+  isLastComment,
+  postId,
+  postUserHandle
 }) => {
+  const db = firebase.firestore();
   const textSize = 1.35;
-  // postingComment?
 
-  const postsDispatch = usePostsDispatch();
-  const { postingComment } = useCommentsState();
-  const textInputDispatch = useTextInputDispatch();
-  //! FIX
-  const postingReply = false; // TODO: REPLACE WITH REPLIES CONTEXT STATE
-  //! FIX
-
-  const [fetchingReplies, setFetchingReplies] = useState(false);
-  const [fetchedReplies, setFetchedReplies] = useState(false);
-  const [showReplies, setShowReplies] = useState(false);
-  // only apply the opacity loader to the last comment item
-  const isLastComment = index === numComments - 1 ? true : false;
+  const repliesDispatch = useRepliesDispatch();
+  const repliesState = useRepliesState();
+  const commentsState = useCommentsState();
+  const { postingComment } = commentsState;
 
   const onViewRepliesClick = async () => {
-    if (comment.numReplies > 0) {
-      // reset all local replies if user clicks on view replies again
-      postsDispatch({
-        type: 'RESET_LOCAL_REPLIES',
-        payload: { postId, commentId: comment.id }
-      });
+    // show/hide replies
+    repliesDispatch({
+      type: 'SHOW_REPLIES',
+      payload: !repliesState.showReplies
+    });
+    repliesDispatch({ type: 'RESET_LOCAL_REPLIES' });
 
-      setShowReplies(!showReplies);
-      // only fetch if we know for a fact that there are replies in the collection
-      if (!fetchedReplies) {
-        setFetchingReplies(true);
-        await getReplies(postsDispatch, postId, comment.id);
-        setFetchedReplies(true);
-        setFetchingReplies(false);
+    if (comment.numReplies > 0) {
+      // do not fetch if we've already done so
+      if (!repliesState.fetchedReplies) {
+        repliesDispatch({ type: 'SET_FETCHING_REPLIES', payload: true });
+        try {
+          const repliesCollection = await db
+            .doc(`posts/${postId}/comments/${comment.id}`)
+            .collection('replies')
+            .get();
+
+          const replies: Reply[] = repliesCollection.docs.map(d => {
+            return {
+              id: d.id,
+              body: d.data().body,
+              createdAt: d.data().createdAt,
+              userHandle: d.data().userHandle
+            };
+          });
+
+          repliesDispatch({ type: 'SET_REPLIES', payload: replies });
+          repliesDispatch({ type: 'SET_FETCHED_REPLIES', payload: true });
+        } catch (err) {
+          // TODO: Handle error
+          console.log(err);
+        }
+
+        repliesDispatch({ type: 'SET_FETCHING_REPLIES', payload: false });
       }
     }
   };
 
-  const onReplyClick = () => {
-    textInputDispatch({ type: 'SET_POST_ID', payload: postId });
-    textInputDispatch({ type: 'SET_COMMENT_ID', payload: comment.id });
-    textInputDispatch({
-      type: 'SET_COMMENT_MODE',
-      payload: CommentMode.REPLY_COMMENT
-    });
-  };
-
-  const renderReplyElement = (reply: Reply, loading: boolean) => (
-    <Wrapper key={uuid()}>
-      <OpacityLoader loading={loading ? 1 : 0} defaultOpacity={1}>
-        <Text size={textSize} weight={700}>
-          {reply.userHandle}{' '}
-        </Text>
-      </OpacityLoader>
-
-      <OpacityLoader loading={loading ? 1 : 0} defaultOpacity={1}>
-        <Text size={textSize}>{reply.body}</Text>
-      </OpacityLoader>
-
-      <Text size={1.2} opacity={0.7}>
-        {moment(reply.createdAt)
-          .local()
-          .fromNow()}
-      </Text>
-    </Wrapper>
-  );
-
   return (
-    <Wrapper>
+    <CommentReplyWrapper>
       {/* we use the index here to figure out which comment to use the loading logic on. any new comment is pushed into the comments array so it'd be at the very last index. we only apply the loading logic on that last comment as it'll be the one being added
        */}
 
@@ -119,46 +100,22 @@ export const CommentItem: React.FC<CommentItemProps> = ({
           .local()
           .fromNow()}{' '}
         |{' '}
-        <ClickableSpan onClick={!postingReply ? onViewRepliesClick : undefined}>
-          {showReplies
+        <ClickableSpan
+          onClick={!repliesState.postingReply ? onViewRepliesClick : undefined}
+        >
+          {repliesState.showReplies
             ? `Hide All Replies (${comment.numReplies})`
             : `View All Replies (${comment.numReplies})`}{' '}
         </ClickableSpan>
-        |{' '}
-        <ClickableSpan onClick={!postingReply ? onReplyClick : undefined}>
-          Reply
-        </ClickableSpan>
       </Text>
 
-      <ReplyForm />
-
-      {/* <RepliesContainer>
-        {showReplies && (
-          <StyledLoadingSpinner loading={fetchingReplies ? 1 : 0} small>
-            <RepliesWrapper>
-              {comment.replies.map((reply, idx) => (
-                <>
-                  {renderReplyElement(
-                    reply,
-                    comment.replies.length - 1 === idx && postingReply
-                  )}
-                </>
-              ))}
-            </RepliesWrapper>
-          </StyledLoadingSpinner>
-        )}
-
-        <RepliesWrapper>
-          {localReplies.map((reply, idx) => (
-            <>
-              {renderReplyElement(
-                reply,
-                comment.replies.length - 1 === idx && postingReply
-              )}
-            </>
-          ))}
-        </RepliesWrapper>
-      </RepliesContainer> */}
-    </Wrapper>
+      {/* Will only show when showReplies is true */}
+      <Replies
+        commentUserHandle={comment.userHandle}
+        postId={postId}
+        commentId={comment.id}
+        postUserHandle={postUserHandle}
+      />
+    </CommentReplyWrapper>
   );
 };
