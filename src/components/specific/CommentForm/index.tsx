b22a02dec7ad50b9ea83f35e4a07511807007136
SKP <skp.frl@gmail.com>
2020-03-14 15:22:02 +0530

    :recycle: Refactor CommentForm component entirely (pretty much a full on rework)

--- src/components/specific/CommentForm/index.tsx
+++ src/components/specific/CommentForm/index.tsx
@@ -1,21 +1,23 @@
-import React, { useState, useEffect, useRef } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
 import uuid from 'uuid/v4';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 import { useUserState } from '../../../contexts/UserContext';
-import { useCommentsDispatch } from '../../../contexts/CommentsContext';
 import {
-  useTextInputState,
-  useTextInputDispatch
-} from '../../../contexts/TextInputContext';
+  useCommentsDispatch,
+  useCommentsState
+} from '../../../contexts/CommentsContext';
 import { SendButton } from '../../shared/SendButton';
-import { API_URL } from '../../../constants/apiUrl';
-import { onGetUserIdToken } from '../../../helpers';
-import { CommentMode } from '../../../types';
-import { Wrapper, TextInput, InputLabel } from './styles';
+import { Text } from '../../shared/Text';
+import { CommentReplyTextArea } from '../../shared/CommentReplyTextArea';
+import { CommentReplyFormContainer } from '../../shared/CommentReplyFormContainer';
+import { OpacityLoader } from '../../shared/OpacityLoader';
+import { Comment } from '../../../types';
+import { Wrapper } from './styles';
 
 type CommentFormProps = {
-  postingComment: boolean;
   postId: string;
+  postUserHandle: string;
 };
 
 /* 
@@ -24,196 +26,85 @@ type CommentFormProps = {
 */
 
 export const CommentForm: React.FC<CommentFormProps> = ({
-  postingComment,
-  postId
+  postId,
+  postUserHandle
 }) => {
+  const db = firebase.firestore();
+
   const [commentText, setCommentText] = useState('');
-  const textInputState = useTextInputState();
-  const textInputDispatch = useTextInputDispatch();
   const { userHandle } = useUserState();
   const commentsDispatch = useCommentsDispatch();
-  const isReplying = textInputState.commentMode === CommentMode.REPLY_COMMENT;
+  const commentsState = useCommentsState();
 
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    /*
-       Focus the input only if post id in context state and post id of the component overlap
-       otherwise we end up focusing the wrong input
-     */
-    if (isReplying && textInputState.postId === postId)
-      inputRef.current && inputRef.current.focus();
-  }, [isReplying, postId, textInputState.postId]);
-
-  const submitComment = () => {
-    const commentId = uuid();
-
-    commentsDispatch({
-      type: 'ADD_COMMENT',
-      payload: {
+  const onCommentSubmit = async () => {
+    if (commentText) {
+      // pass by value
+      const commentInput = commentText;
+      // reset input
+      setCommentText('');
+      const commentId = uuid();
+      const newComment: Comment = {
         id: commentId,
         userHandle,
-        body: commentText,
+        body: commentInput,
         createdAt: new Date().toISOString(),
         numReplies: 0
+      };
+
+      commentsDispatch({
+        type: 'ADD_COMMENT',
+        payload: newComment
+      });
+
+      commentsDispatch({
+        type: 'SET_POSTING_COMMENT',
+        payload: true
+      });
+
+      try {
+        await db
+          .doc(`users/${postUserHandle}/posts/${postId}`)
+          .collection('comments')
+          .doc(commentId)
+          .set(newComment);
+      } catch (err) {
+        // TODO: Handle error
+        console.log(err);
       }
-    });
 
-    // onGetUserIdToken(async idToken => {
-    //   try {
-    //     postsDispatch({
-    //       type: 'SET_POSTING_COMMENT',
-    //       payload: {
-    //         postId,
-    //         value: true
-    //       }
-    //     });
-
-    //     await axios.post(
-    //       `${API_URL}/posts/add-comment`,
-    //       {
-    //         commentId,
-    //         postId,
-    //         commentBody: commentText
-    //       },
-    //       {
-    //         headers: {
-    //           Authorization: `Bearer ${idToken}`
-    //         }
-    //       }
-    //     );
-
-    //     postsDispatch({
-    //       type: 'SET_POSTING_COMMENT',
-    //       payload: {
-    //         postId,
-    //         value: false
-    //       }
-    //     });
-    //   } catch (err) {
-    //     // TODO: HANDLE ERROR
-    //     console.log(err);
-    //   }
-    // });
-  };
-
-  const submitReply = () => {
-    const { commentId } = textInputState;
-    const replyId = uuid();
-
-    // postsDispatch({
-    //   type: 'ADD_REPLY',
-    //   payload: {
-    //     replyId,
-    //     commentId,
-    //     postId,
-    //     body: commentText,
-    //     userHandle
-    //   }
-    // });
-
-    // onGetUserIdToken(async idToken => {
-    //   try {
-    //     // disable all inputs while we're adding the reply
-    //     postsDispatch({
-    //       type: 'SET_POSTING_COMMENT',
-    //       payload: {
-    //         postId,
-    //         value: true
-    //       }
-    //     });
-    //     postsDispatch({
-    //       type: 'SET_POSTING_REPLY',
-    //       payload: {
-    //         postId,
-    //         commentId,
-    //         value: true
-    //       }
-    //     });
-
-    //     await axios.post(
-    //       `${API_URL}/posts/comments/replies`,
-    //       {
-    //         replyId,
-    //         commentId,
-    //         postId,
-    //         replyBody: commentText
-    //       },
-    //       {
-    //         headers: {
-    //           Authorization: `Bearer ${idToken}`
-    //         }
-    //       }
-    //     );
-    //   } catch (err) {
-    //     // TODO: HANDLE ERROR
-    //     console.log(err);
-    //   }
-
-    //   postsDispatch({
-    //     type: 'SET_POSTING_COMMENT',
-    //     payload: {
-    //       postId,
-    //       value: false
-    //     }
-    //   });
-    //   postsDispatch({
-    //     type: 'SET_POSTING_REPLY',
-    //     payload: {
-    //       postId,
-    //       commentId,
-    //       value: false
-    //     }
-    //   });
-    // });
-  };
-
-  const onCommentSubmit = () => {
-    if (commentText) {
-      if (isReplying) submitReply();
-      else submitComment();
+      commentsDispatch({ type: 'SET_POSTING_COMMENT', payload: false });
     }
-
-    setCommentText('');
   };
 
-  // switches mode to post comment
-  const switchCommentMode = () => {
-    textInputDispatch({
-      type: 'SET_COMMENT_MODE',
-      payload: CommentMode.POST_COMMENT
-    });
-  };
-
-  const onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const onTextAreaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setCommentText(e.target.value);
-    if (
-      e.target.value === '' &&
-      textInputState.commentMode === CommentMode.REPLY_COMMENT
-    )
-      switchCommentMode();
-  };
-
-  const onPressEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') onCommentSubmit();
   };
 
   return (
     <Wrapper>
-      <TextInput
-        type='text'
-        placeholder={isReplying ? 'Reply to comment' : 'Add a comment'}
-        value={commentText}
-        onClick={switchCommentMode}
-        onChange={onInputChange}
-        onKeyPress={onPressEnter}
-        disabled={postingComment}
-        ref={inputRef}
-      />
-      <InputLabel>
-        {isReplying ? 'Replying to a comment' : 'Commenting on a post'}
-      </InputLabel>
-      <SendButton onClick={onCommentSubmit} />
+      <OpacityLoader
+        loading={commentsState.postingComment ? 1 : 0}
+        defaultOpacity={1}
+      >
+        <Text size={1.35} weight={700}>
+          {userHandle}
+        </Text>
+        <CommentReplyFormContainer>
+          <CommentReplyTextArea
+            maxRows={5}
+            maxLength={1500}
+            placeholder='Add a comment'
+            value={commentText}
+            onChange={onTextAreaChange}
+            disabled={commentsState.postingComment}
+            required
+          />
+          <SendButton
+            onClick={onCommentSubmit}
+            disabled={commentsState.postingComment}
+          />
+        </CommentReplyFormContainer>
+      </OpacityLoader>
     </Wrapper>
   );
 };
