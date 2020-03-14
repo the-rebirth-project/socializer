d391d254e293a0305c4b3a9dfcf4417921ccf501
SKP <skp.frl@gmail.com>
2020-03-14 15:19:46 +0530

    :lipstick: Remove scrollbar for comment and reply form text area

--- src/components/shared/CommentReplyTextArea/index.ts
+++ src/components/shared/CommentReplyTextArea/index.ts
@@ -1,23 +1,23 @@
 import styled from 'styled-components';
 import AutosizeTextarea from 'react-textarea-autosize';
-import { scrollbarStyles } from '../../../constants';
 
-export const CommentReplyTextArea = styled.textarea`
+export const CommentReplyTextArea = styled(AutosizeTextarea)`
   width: 100%;
-  height: 6rem;
   border: none;
   background-color: transparent;
-  padding: 0rem;
   font-family: inherit;
   font-size: 1.35rem;
   color: ${props => props.theme.colors.textColor};
   resize: none;
   line-height: 1.35;
+  padding: 0rem;
   padding-right: 0.5rem;
 
   :focus {
     outline: none;
   }
 
-  ${scrollbarStyles}
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
