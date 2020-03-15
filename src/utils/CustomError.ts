1e4b2ecda127da1bc12f73b78b2c585d972b3f8b
SKP <skp.frl@gmail.com>
2020-03-15 23:14:36 +0530

    :truck: Rename AuthError util file to CustomError

--- src/utils/CustomError.ts
+++ src/utils/CustomError.ts
@@ -1,14 +1,14 @@
-export class AuthError extends Error {
+export class CustomError extends Error {
   code: string;
   constructor(code: string, ...params: any[]) {
     super(...params);
 
     // Maintains proper stack trace for where our error was thrown (only available on V8)
     if (Error.captureStackTrace) {
-      Error.captureStackTrace(this, AuthError);
+      Error.captureStackTrace(this, CustomError);
     }
 
-    this.name = 'AuthError';
+    this.name = 'CustomError';
     this.code = code;
   }
 }
