1d566d6c66a0e2d5892012bb989dd1d43e63b2c2
SKP <skp.frl@gmail.com>
2020-03-15 10:35:48 +0530

    :lipstick: Fix post items being too slim

--- src/components/specific/UserPosts/styles.ts
+++ src/components/specific/UserPosts/styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const Wrapper = styled.main`
   display: grid;
   grid-template-rows: auto;
+  grid-template-columns: 34rem;
   row-gap: 4rem;
   justify-content: center;
 `;
