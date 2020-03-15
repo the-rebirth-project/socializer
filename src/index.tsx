7f63299cb14c52d527a37ec9f6d18b57e333786a
SKP <skp.frl@gmail.com>
2020-03-15 09:22:46 +0530

    :construction: test firebase config settings

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import { ThemeProvider } from 'styled-components';
 import { mainTheme } from './themes/mainTheme';
 
 const FBConfig = require('./config/index');
-console.log(FBConfig.API_KEY);
+console.log(process.env);
 firebase.initializeApp(FBConfig);
 
 ReactDOM.render(
