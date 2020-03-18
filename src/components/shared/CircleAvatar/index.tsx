5de06c5153fff49937dd74d5fa3d3efb64ffcb94
SKP <skp.frl@gmail.com>
2020-03-18 23:14:46 +0530

    :pencil: Document props of CircleAvatar shared component

--- src/components/shared/CircleAvatar/index.tsx
+++ src/components/shared/CircleAvatar/index.tsx
@@ -3,7 +3,15 @@ import noProfileImg from '../../../img/no-img.png';
 import { Wrapper } from './styles';
 
 type CircleAvatarProps = {
+  /**
+   * The URL of the image to be rendered
+   */
   imgUrl: string;
+  /**
+   * Determines the size of the avatar. The value provided here multiplies the default size.
+   *
+   * Default size of avatar in rem units is 4.15rem
+   */
   sizeScaling?: number;
 };
 
