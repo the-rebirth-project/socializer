f1126d014d703089b39878177bce8207d24414a0
SKP <skp.frl@gmail.com>
2020-03-24 11:05:54 +0530

    :iphone: Make HomeView component more responsive

--- src/views/HomeView/styles.ts
+++ src/views/HomeView/styles.ts
@@ -1,8 +1,8 @@
 import styled, { createGlobalStyle } from 'styled-components';
+import { device } from '../../utils/responsive';
 
 export const GlobalStyles = createGlobalStyle`
   body {
-    overflow-x: hidden;
     background-color: ${props => props.theme.colors.background};
     line-height: 1.35;
 
@@ -14,7 +14,7 @@ export const GlobalStyles = createGlobalStyle`
 `;
 
 export const ContentWrapper = styled.main`
-  width: 100vw;
+  width: 100%;
   height: fit-content;
   display: flex;
   justify-content: center;
@@ -24,6 +24,11 @@ export const GridContainer = styled.div`
   display: grid;
   grid-auto-rows: min-content;
   row-gap: 4rem;
-  grid-template-columns: 34rem;
-  justify-items: center;
+  grid-template-columns: 90%;
+  justify-content: center;
+
+  /* make it fixed for devices larger than mobile */
+  @media ${device.mobileL} {
+    grid-template-columns: 34rem;
+  }
 `;
