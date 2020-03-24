4a9f3c9454530bed2dd3a25e0f9845535dd92b11
SKP <skp.frl@gmail.com>
2020-03-24 11:06:54 +0530

    :lipstick: Style RegisterView component

--- src/views/RegisterView/styles.ts
+++ src/views/RegisterView/styles.ts
@@ -1,18 +1,10 @@
-import styled, { createGlobalStyle } from 'styled-components';
+import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  height: 100vh;
-  width: 100vw;
-  padding: 6.5rem 0rem;
   background-color: ${props => props.theme.colors.background};
   display: grid;
-  grid-template-rows: 1fr 2.7fr 1.6fr;
+  grid-template-rows: auto;
   row-gap: 4rem;
   justify-content: center;
-`;
-
-export const GlobalBackground = createGlobalStyle`
-  body {
-    background-color: ${props => props.theme.colors.background};
-  }
+  justify-items: center;
 `;
