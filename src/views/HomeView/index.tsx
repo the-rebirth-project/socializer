280669b0226a27ac4da69ad24fef9d5faaf8bb1f
SKP <skp.frl@gmail.com>
2020-03-24 11:05:30 +0530

    :lipstick: Implement Page component in HomeView

--- src/views/HomeView/index.tsx
+++ src/views/HomeView/index.tsx
@@ -1,28 +1,14 @@
-import React, { useEffect } from 'react';
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import { RouteComponentProps, navigate } from '@reach/router';
+import React from 'react';
+import { RouteComponentProps } from '@reach/router';
 import { PostsProvider } from '../../contexts/PostsContext';
-import { Navbar, NavbarBottomMargin } from '../../components/shared/Navbar';
-import { SizedBox } from '../../components/shared/SizedBox';
+import { Page } from '../../components/shared/Page';
 import { Posts } from '../../components/specific/Posts';
 import { GlobalStyles, ContentWrapper, GridContainer } from './styles';
 
 export const HomeView: React.FC<RouteComponentProps> = () => {
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged(user => {
-      if (!user) {
-        navigate('/login');
-      }
-    });
-  }, []);
-
   return (
-    <div>
+    <Page>
       <GlobalStyles />
-      <header style={{ marginBottom: `${NavbarBottomMargin}rem` }}>
-        <Navbar />
-      </header>
       <PostsProvider>
         <ContentWrapper>
           <GridContainer>
@@ -30,7 +16,6 @@ export const HomeView: React.FC<RouteComponentProps> = () => {
           </GridContainer>
         </ContentWrapper>
       </PostsProvider>
-      <SizedBox height={5} />
-    </div>
+    </Page>
   );
 };
