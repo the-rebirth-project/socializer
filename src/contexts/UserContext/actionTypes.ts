60c8540c054722d6ac2d5f655f824efdfe1532bf
SKP <skp.frl@gmail.com>
2020-03-24 11:00:47 +0530

    :label: Add ChangeProfilePhoto action type in UserContext

--- src/contexts/UserContext/actionTypes.ts
+++ src/contexts/UserContext/actionTypes.ts
@@ -1,11 +1,23 @@
-export type Action = SetUser | SetFetchingUser;
+export type Action = SetUser | SetFetchingUser | ChangeProfilePhoto;
 
 type SetUser = {
   type: 'SET_USER';
   payload: {
+    email: string;
+    userId: string;
     userHandle: string;
     userProfile: string;
+    bio: string;
+    location: string;
   };
+};
+
+type ChangeProfilePhoto = {
+  type: 'CHANGE_PROFILE_PHOTO';
+  /**
+   * The payload contains the image URL of the new profile photo
+   */
+  payload: string;
 };
 
 type SetFetchingUser = {
