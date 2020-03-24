685b88df9f086f34b4b83fa00d1035b0fa643f5a
SKP <skp.frl@gmail.com>
2020-03-24 10:52:06 +0530

    :bug: Fix memleak issue

--- src/components/specific/UserInfo/index.tsx
+++ src/components/specific/UserInfo/index.tsx
@@ -8,6 +8,7 @@ import {
 } from '../../../contexts/UserProfileContext';
 import { UserInfoCard } from '../UserInfoCard';
 import { UserPosts } from '../UserPosts';
+import { useMounted } from '../../../hooks/useMounted';
 import { Text } from '../../shared/Text';
 import { LoadingSpinner } from '../../shared/LoadingSpinner';
 import { SubHeading } from '../../shared/SubHeading';
@@ -20,6 +21,7 @@ type UserInfoProps = {
 };
 export const UserInfo: React.FC<UserInfoProps> = ({ userHandle }) => {
   const db = firebase.firestore();
+  const isMounted = useMounted();
   const userState = useUserState();
   const userProfileState = useUserProfileState();
   const userProfileDispatch = useUserProfileDispatch();
@@ -28,40 +30,54 @@ export const UserInfo: React.FC<UserInfoProps> = ({ userHandle }) => {
 
   const fetchUser = async (): Promise<void> => {
     try {
-      userProfileDispatch({ type: 'SET_FETCHING_DATA', payload: true });
-      const userData = await db
+      isMounted.current &&
+        userProfileDispatch({ type: 'SET_FETCHING_DATA', payload: true });
+      const snap = await db
         .collection('users')
-        .doc(userHandle)
+        .where('userHandle', '==', userHandle)
         .get();
+      const userData = snap.docs[0];
 
-      if (userData.exists) {
+      if (userData !== undefined && userData.exists) {
         const data = userData.data();
         const user: UserData = {
           userHandle: data?.userHandle,
+          userId: data?.userId,
           bio: data?.bio,
           numSeeds: data?.numSeeds,
-          numSubscribers: data?.numSubscribers,
+          // decrement numSubscribers to offset the fact that you automatically sub to yourself
+          numSubscribers: data?.numSubscribers - 1,
           numPosts: data?.numPosts,
           location: data?.location,
           profileImageURL: data?.profileImageURL
         };
-        userProfileDispatch({ type: 'SET_USER_PROFILE', payload: user });
+        isMounted.current &&
+          userProfileDispatch({ type: 'SET_USER_PROFILE', payload: user });
 
         if (userState.userHandle) {
+          // check to see if user is authorized to edit profile info
+          if (userState.userHandle === user.userHandle)
+            isMounted.current &&
+              userProfileDispatch({
+                type: 'SET_AUTHORIZED_TO_EDIT',
+                payload: true
+              });
+
           const subscriberDoc = await db
             .collection('users')
-            .doc(userHandle)
+            .doc(user.userId)
             .collection('subscribers')
-            .doc(userState.userHandle)
+            .doc(userState.userId)
             .get();
           // current user is subscribed if the doc exists
-          userProfileDispatch({
-            type: 'SET_IS_SUBSCRIBED',
-            payload: subscriberDoc.exists
-          });
+          isMounted.current &&
+            userProfileDispatch({
+              type: 'SET_IS_SUBSCRIBED',
+              payload: subscriberDoc.exists
+            });
         }
       } else {
-        setHasError(true);
+        isMounted.current && setHasError(true);
       }
     } catch (err) {
       // Probably a network error
@@ -69,10 +85,14 @@ export const UserInfo: React.FC<UserInfoProps> = ({ userHandle }) => {
       console.log(err);
     }
 
-    userProfileDispatch({ type: 'SET_FETCHING_DATA', payload: false });
+    isMounted.current &&
+      userProfileDispatch({ type: 'SET_FETCHING_DATA', payload: false });
   };
 
-  const memoizedFetchUser = useCallback(fetchUser, [userState.userHandle]);
+  const memoizedFetchUser = useCallback(fetchUser, [
+    userState.userHandle,
+    isMounted
+  ]);
 
   useEffect(() => {
     memoizedFetchUser();
