94d963bdcd2c24113f7e53d255b4fd19548f58d9
SKP <skp.frl@gmail.com>
2020-03-24 10:54:45 +0530

    :label: Add SetNumComments action

--- src/contexts/CommentsContext/actionTypes.ts
+++ src/contexts/CommentsContext/actionTypes.ts
@@ -1,10 +1,21 @@
 import { Comment } from '../../types';
 
-export type Action = AddComment | SetPostingComment | SetComments;
+export type Action =
+  | AddComment
+  | SetPostingComment
+  | SetComments
+  | SetNumComments
+  | IncrementNumComments
+  | DecrementNumComments;
 
 type SetComments = {
   type: 'SET_COMMENTS';
   payload: Comment[];
+};
+
+type SetNumComments = {
+  type: 'SET_NUM_COMMENTS';
+  payload: number;
 };
 
 type SetPostingComment = {
