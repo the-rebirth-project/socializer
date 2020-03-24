9751eb6c858a328bacaf5fc1caeec953d1f74f44
SKP <skp.frl@gmail.com>
2020-03-24 10:40:23 +0530

    :iphone: Make Page component responsive

--- src/components/shared/Page/index.ts
+++ src/components/shared/Page/index.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { navbarHeight } from '../../../constants/navbarHeight';
+import { device } from '../../../utils/responsive';
 
 export const Page = styled.div`
   /* Provides scrolling mechanism */
@@ -7,4 +8,9 @@ export const Page = styled.div`
   width: 100vw;
   height: 100vh;
   padding: ${navbarHeight * 2}rem 0rem;
+
+  @media ${device.laptop} {
+    padding-top: ${navbarHeight * 2}rem;
+    padding-bottom: 4rem;
+  }
 `;
