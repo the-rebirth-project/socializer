154abb957fcf99f681a0ec7b9300ae6078894f63
SKP <skp.frl@gmail.com>
2020-03-24 11:06:17 +0530

    :lipstick: Implement Page component in LoginView

--- src/views/LoginView/index.tsx
+++ src/views/LoginView/index.tsx
@@ -2,16 +2,26 @@ import React from 'react';
 import { RouteComponentProps } from '@reach/router';
 import { LoginForm } from '../../components/specific/LoginForm';
 import { SubHeading } from '../../components/shared/SubHeading';
-import { Wrapper, GlobalBackground } from '../RegisterView/styles';
+import { Page } from '../../components/shared/Page';
+import { Wrapper } from '../RegisterView/styles';
 
-export const LoginView: React.FC<RouteComponentProps> = () => {
+export const LoginView: React.FC<RouteComponentProps> = ({ path }) => {
   return (
-    <>
-      <GlobalBackground />
+    <Page>
       <Wrapper>
-        <SubHeading>Enter Credentials</SubHeading>
-        <LoginForm />
+        {path === 'login' && (
+          <>
+            <SubHeading>Sign In</SubHeading>
+            <LoginForm />
+          </>
+        )}
+        {path === 'reauthenticate' && (
+          <>
+            <SubHeading>Reauthenticate</SubHeading>
+            <LoginForm reauthenticate />
+          </>
+        )}
       </Wrapper>
-    </>
+    </Page>
   );
 };
