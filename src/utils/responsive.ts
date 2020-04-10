1e0839ad2f261b39477db45d972b1304a4ab5d6a
SKP <skp.frl@gmail.com>
2020-04-10 13:31:23 +0530

    :bug: Fix desktopL media query

--- src/utils/responsive.ts
+++ src/utils/responsive.ts
@@ -5,16 +5,17 @@ const size = {
   tablet: '768px',
   laptop: '1024px',
   laptopL: '1440px',
-  desktop: '2560px'
+  desktop: '1920px',
+  desktopL: '2560px',
 };
 
 export const device = {
-  mobileS: `only screen and (min-width: ${size.mobileS})`,
-  mobileM: `only screen and (min-width: ${size.mobileM})`,
-  mobileL: `only screen and (min-width: ${size.mobileL})`,
-  tablet: `only screen and (min-width: ${size.tablet})`,
-  laptop: `only screen and (min-width: ${size.laptop})`,
-  laptopL: `only screen and (min-width: ${size.laptopL})`,
-  desktop: `only screen and (min-width: ${size.desktop})`,
-  desktopL: `only screen and (min-width: ${size.desktop})`
+  mobileS: `screen and (min-width: ${size.mobileS})`,
+  mobileM: `screen and (min-width: ${size.mobileM})`,
+  mobileL: `screen and (min-width: ${size.mobileL})`,
+  tablet: `screen and (min-width: ${size.tablet})`,
+  laptop: `screen and (min-width: ${size.laptop})`,
+  laptopL: `screen and (min-width: ${size.laptopL})`,
+  desktop: `screen and (min-width: ${size.desktop})`,
+  desktopL: `screen and (min-width: ${size.desktopL})`,
 };
