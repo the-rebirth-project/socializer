ff5d5d66671eea52ab99349119f5d6ed8c774ca2
SKP <skp.frl@gmail.com>
2020-04-10 13:33:26 +0530

    :pencil: Add documentation for Notification type

--- src/types/Notification.ts
+++ src/types/Notification.ts
@@ -8,6 +8,9 @@ export type Notification = {
    * The profile image URL of the user who performed the action
    */
   userProfile: string;
+  /**
+   * Notification message. Goes right after the username.
+   */
   message: string;
   createdAt: string;
 };
