79a3725694d47f41dcc51a61b0c8f72f25e0fd19
SKP <skp.frl@gmail.com>
2020-04-12 15:39:28 +0530

    :lipstick: Make button UI change when the subscribe button is pressed

--- src/components/shared/SecondaryButton/index.ts
+++ src/components/shared/SecondaryButton/index.ts
@@ -15,31 +15,31 @@ export const SecondaryButton = styled.button<SecondaryButtonProps>`
   width: 100%;
   height: 4.4rem;
   text-transform: uppercase;
-  transition: all 0.2s ease-in-out;
+  transition: all 0.25s ease-in-out;
   display: flex;
   justify-content: center;
   align-items: center;
 
-  ${props => {
-      const color = props.serious
-        ? props.theme.colors.tertiary
-        : props.theme.colors.secondary;
-      return css`
-        border: 2px solid ${color};
-        box-shadow: 0px 3px 6px ${color}40;
-        color: ${color};
+  ${(props) => {
+    const color = props.serious
+      ? props.theme.colors.tertiary
+      : props.theme.colors.secondary;
+    return css`
+      border: 2px solid ${color};
+      box-shadow: 0px 3px 6px ${color}40;
+      color: ${color};
 
-        :hover {
-          transform: translateY(-1px);
-          box-shadow: 0px 6px 40px ${color}40;
-        }
+      :hover {
+        transform: translateY(-1px);
+        box-shadow: 0px 6px 40px ${color}40;
+      }
 
-        :active {
-          transform: translateY(0.5px);
-        }
-      `;
-    }}
-    :disabled {
+      :active {
+        transform: translateY(0.5px);
+      }
+    `;
+  }}
+  :disabled {
     cursor: default;
     opacity: 0.7;
     box-shadow: none;
@@ -58,7 +58,7 @@ export const SecondaryButton = styled.button<SecondaryButtonProps>`
     outline: none;
   }
 
-  ${props =>
+  ${(props) =>
     props.small &&
     css`
       height: 3rem;
